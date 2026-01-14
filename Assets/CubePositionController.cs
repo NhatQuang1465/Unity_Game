809932d60508e7f3047a50545e34302b124d2b6f
Nguyen Nhat Quang <nhatquang1424@gmail.com>
2026-01-14 18:12:26 +0700

    upload file

---
+++ Assets/CubePositionController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CubePositionController : MonoBehaviour
+{
+    public enum Mode { Center, BottomLeft }
+    public Mode mode = Mode.Center;
+    public float distance = 5f;
+
+    void Start()
+    {
+        Camera cam = Camera.main;
+
+        if (mode == Mode.Center)
+        {
+            Vector3 pos = new Vector3(Screen.width / 2f, Screen.height / 2f, distance);
+            transform.position = cam.ScreenToWorldPoint(pos);
+        }
+        else if (mode == Mode.BottomLeft)
+        {
+            Vector3 pos = new Vector3(0f, 0f, distance);
+            transform.position = cam.ScreenToWorldPoint(pos);
+        }
+    }
+}
